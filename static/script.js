ea791c72cc0aa6ca250713b8dccebe9dfc9d63da
Abhishek Bhoyar <abhibhoyar141@gmail.com>
2023-09-19 15:51:46 +0530

    adding js

---
+++ static/script.js
@@ -0,0 +1,14 @@
+function animateButton() {
+    const button = document.getElementById("goButton");
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+
+    button.style.transition = "all 15s";
+    // button.style.fontSize = "12px";
+    button.innerHTML = "ITECHROOTS"
+    button.style.transform = `scale(${Math.max(windowWidth, windowHeight)})`;
+
+    setTimeout(() => {
+        window.location.href = "home.html"; // Change this to your home page URL
+    }, 1000);
+}
