8206434bffed3049eea3aa4cf93d18df3ca9b214
Yash Dnyaneshwar Shelke <yashshelke885@gmail.com>
2024-08-12 16:53:56 +0530

    done

---
+++ static/js/952.2b99fb40.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkitech=self.webpackChunkitech||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.2b99fb40.chunk.js.map
